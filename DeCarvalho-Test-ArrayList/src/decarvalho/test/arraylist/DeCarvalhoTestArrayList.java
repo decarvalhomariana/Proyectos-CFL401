7c45bc0e82ebb7a5a35ce4aceeaea657bd620ed3
Programación <Programacin@DESKTOP-L1H5F7B>
2019-07-01 21:24:52 -0300

    Test-ArrayList: Inicio de proyecto, creado de ArrayList Probar y arreglar el tema del try-catch

--- DeCarvalho-Test-ArrayList/src/decarvalho/test/arraylist/DeCarvalhoTestArrayList.java
+++ DeCarvalho-Test-ArrayList/src/decarvalho/test/arraylist/DeCarvalhoTestArrayList.java
@@ -5,6 +5,9 @@
  */
 package decarvalho.test.arraylist;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Programación
@@ -15,7 +18,50 @@ public class DeCarvalhoTestArrayList {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HOlis");
+        
+    Scanner entrada = new Scanner(System.in);
+    ArrayList elem = new ArrayList();
+    int numero;
+    boolean bandera = true;
+    
+    elem.add("Perro");
+    elem.add(4);
+    elem.add(7);
+    
+    System.out.println(elem);
+    
+    ArrayList <Integer> elem2 = new ArrayList();
+    
+    //MI opción
+    while (bandera){
+    System.out.print("Ingrese un número: ");
+    numero = entrada.nextInt();
+    try{
+        numero = entrada.nextInt();
+    }catch (Exception e){
+        System.out.println("Error");
+    }
+        if (numero>=0){
+            elem2.add(numero);
+        }else{ 
+            bandera = false; 
+            System.out.println(elem2);
+    }
+    }
+     
+    /*Segunda opción
+    numero = entrada.nextInt();
+    while (numero>=0){
+        elem2.add(numero);
+        numero = entrada.nextInt();
+    }
+    System.out.println(elem2);
+    */
+
+    ArrayList animales = new ArrayList();
+    
+    
+    
     }
     
 }
