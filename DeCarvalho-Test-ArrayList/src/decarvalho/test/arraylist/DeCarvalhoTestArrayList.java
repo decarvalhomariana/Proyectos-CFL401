7bdbc2f8ddfaceee3dc0c83fd2042748f6298021
Programación <Programacin@DESKTOP-L1H5F7B>
2019-07-12 20:40:07 -0300

    Test-ArrayList: Inicio de proyecto, creado de ArrayList Probar y arreglar el tema del try-catch Seguimos con Listas, agregamos iterador. agregamos documentacion creacion de otra clase, intercambio de informacion entre clases. Manejo de archivos de texto

--- DeCarvalho-Test-ArrayList/src/decarvalho/test/arraylist/DeCarvalhoTestArrayList.java
+++ DeCarvalho-Test-ArrayList/src/decarvalho/test/arraylist/DeCarvalhoTestArrayList.java
@@ -5,9 +5,15 @@
  */
 package decarvalho.test.arraylist;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -18,7 +24,7 @@ public class DeCarvalhoTestArrayList {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {/*
+    public static void main(String[] args) throws IOException {/*
         
     Scanner entrada = new Scanner(System.in);
     ArrayList elem = new ArrayList();
@@ -110,7 +116,7 @@ public class DeCarvalhoTestArrayList {
         //System.out.println(((Mascotas) it.next()).getNombre());
         System.out.println("El nombre de su mascota es "+ mascota.getNombre()+ " la edad " + mascota.getEdad() + " y la cantidad de patas que tiene son "+ mascota.getPatas()+ ".");
     }
-    */
+    
     //Creacion de objetos.
     Persona per1 = new Persona("José Luís", 45);
     Persona per2 = new Persona ("Marta", 53);
@@ -133,6 +139,79 @@ public class DeCarvalhoTestArrayList {
     
     System.out.println(per2.cuantasMascotasTenes());
     System.out.println(per1.cuantasMascotasTenes());
+*/
+    
+    //FileWriter, es la clase que me permite comunicarme con un ARCHIVO
+    //FileWriter file(nombre variable) = new FileWriter(ruta al archivo, si se sobre escrive o no)
+    
+    FileWriter file = new FileWriter ("texto.txt", false); //el false hace que se sobre escriba, el true hace que se sigan creando archivos. Si no esta el archivo lo genera.
+    PrintWriter escritor = new PrintWriter (file);//Manejador. Maneja las interaccionas. poniendo el punto se accede a las acciones que se pueden hacer.
+    escritor.println("¿Cómo estás?");
+    escritor.print("Edad: ");
+    //escritor.print(JOptionPane.showInputDialog(null, "Escribi algo"));//lo que escriba el usuario queda guardado en el archivo.
+    
+    //antes del close se hace un flush, las partes que quedan en el buffer que corresponder al arcivos las manda al archivo.
+    escritor.flush();
+    escritor.close();// así se cierra el archivo.
+   
+    //para leer el archivo. Generamos el link al archivo.
+    FileReader fileR = new FileReader ("texto.txt");
+    BufferedReader lector = new BufferedReader (fileR);//el que me permite utilizar el link.
+    
+    //Para leer entradas de texto
+    String textoEntrada = lector.readLine();//lee por líneas. Guarda en la variable textoEntrada lo que lee.
+    //Cuando quiera leer algo y no hay nada va a llenar la variable con el valor null.
+    
+    while (textoEntrada != null){
+        System.out.println(textoEntrada);
+        textoEntrada = lector.readLine();//para que lea la siguiente línea, sino va a seguir leyendo la primer linea del archivo infinitamente.
+        
+    }
+    
+    lector.close();
+    
+    
+    FileWriter conver = new FileWriter ("Conver.txt", true);
+    PrintWriter escritor2 = new PrintWriter (conver);
+    escritor2.println("Tomás: Hola");
+    escritor2.println("Franco: Hola");
+    escritor2.println("Franco: Todo bien?");
+    escritor2.println("Tomás: Me confundí");
+    escritor2.println("Tomás: Chau");
+    
+    escritor2.flush();
+    escritor2.close();
+    
+    FileReader fileRR = new FileReader ("conver.txt");
+    BufferedReader lector2 = new BufferedReader (fileRR);
+    textoEntrada = lector2.readLine();
+    //Para separar cosas en Java tenemos el split.
+    
+    while (textoEntrada != null){
+    String[] tP = textoEntrada.split(":");//Transformamos la primer fila de la conversacion en un array para poder separar
+    System.out.println(tP [0]);
+    System.out.println(tP [1]+ "\n");
+    //System.out.println("");
+    //System.out.println("\n" + tP[0] + "\n" + tP[1] );
+    textoEntrada = lector2.readLine();
+    
+    
+    //La documentación java generalmente se hace antes de cada método
+    /**Imprime el documento en la impresora seleccionada y necesita
+     * por paramétro un documento. (@param documento)
+     * @param documento
+     * @param nombreVariable si hay más parámetro en el metódo.
+     * @return valor de si imprimió o no
+     */
+    
+    //alt+shift+f acomoda el codigo.
+    
+    
+    
+    
+    
+    }
+    
     
     
     
